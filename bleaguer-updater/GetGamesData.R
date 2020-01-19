7bbaaa77a0ad1b264167b83e0622b5605357c321
Rintaro Masuda <rintaromasuda@live.com>
2020-01-19 23:22:27 +0900

    In progress

--- bleaguer-updater/GetGamesData.R
+++ bleaguer-updater/GetGamesData.R
@@ -1,133 +1,154 @@
-GetGamesData <- function(webDr){
+.ScrapeGamePage <- function(webDr, urlGame){
+  webDr$navigate(urlGame)
+  Sys.sleep(0.5) # Consider making this event-based
+  
+  pageSource <- webDr$getPageSource()
+  htmlGame <- read_html(pageSource[[1]], encoding = "utf-8")
+  
+  ########
+  # Parsing all the necessary information
+  ########
+  date <- htmlGame %>%
+    html_nodes("#game__top__inner > div.date_wrap > p:nth-child(2) > span") %>%
+    html_text()
+  
+  arena <- htmlGame %>%
+    html_nodes("#game__top__inner > div.place_wrap > p.StadiumNameJ") %>%
+    html_text()
+  arena <- gsub("会場：", "", arena)
+  
+  attendance <- htmlGame %>%
+    html_nodes("#game__top__inner > div.place_wrap > p.Attendance") %>%
+    html_text()
+  attendance <- as.integer(gsub("人", "", gsub("人数：", "", attendance)))  
+
+  home <- htmlGame %>%
+    html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.home.win > div.team_name > p.for-sp") %>%
+    html_text()
+  if (identical(home, character(0))) {
+    home <- htmlGame %>%
+      html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.home > div.team_name > p.for-sp") %>%
+      html_text()
+  }
+  
+  away <- htmlGame %>%
+    html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.away.win > div.team_name > p.for-sp") %>%
+    html_text()
+  if (identical(away, character(0))) {
+    away <- htmlGame %>%
+      html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.away > div.team_name > p.for-sp") %>%
+      html_text()
+  }
+  
+  result <- data.frame(
+    Date = date,
+    Arena = arena,
+    Attendance = attendance,
+    HomeTeam = home,
+    AwayTeam = away
+  )
+  
+  return(result)
+}
+
+GetGamesData <- function(webDr, season){
   library(bleaguer)
   library(rvest)
   
-  df.result <- data.frame()
+  result <- data.frame()
   
-  season <- "2019-20"
   leagues <- c("B1", "B2")
-  scheduleKeys <- subset(b.games, Season == season)$ScheduleKey
-  
   b1.events <- c(2)
   b2.events <- c(7)
-  
-  for (league in leagues) {
-    # Target relevant events only
-    if (league == "B1") {
-      events <- subset(b.events, EventId %in% b1.events)
-    } else {
-      events <- subset(b.events, EventId %in% b2.events)
-    }
-    # Retrieve teams
-    teams <- subset(b.teams, Season == season & League == league)
+
+  keysInData <- subset(bleaguer::b.games, Season == season)$ScheduleKey
     
-    for (event_row in seq(1:nrow(events))) {
-      # Iterate each event
-      event.Id <- events[event_row, ]$EventId
-      event.Name <- events[event_row, ]$ShortName
-      event.Category <- events[event_row, ]$Category
+  for (league in leagues) {
+    # Target events
+    if (league == "B1") {
+      events <- subset(bleaguer::b.events, EventId %in% b1.events)
+    } else {
+      events <- subset(bleaguer::b.events, EventId %in% b2.events)
+    }
+
+    # Target teams
+    teams <- subset(bleaguer::b.teams, Season == season & League == league)
+    
+    for (eventRow in seq(1:nrow(events))) {
+      eventId <- events[eventRow, ]$EventId
+      eventName <- events[eventRow, ]$ShortName
+      eventCategory <- events[eventRow, ]$Category
       
-      for (team_row in seq(1:nrow(teams))) {
-        # Iterate each team
-        team.Id <- teams[team_row, ]$TeamId
-        team.Name <- teams[team_row, ]$NameShort
-        url.team <- paste("https://www.bleague.jp/schedule/?",
+      for (teamRow in seq(1:nrow(teams))) {
+        teamId <- teams[teamRow, ]$TeamId
+        teamName <- teams[teamRow, ]$NameShort
+        urlTeam <- paste0("https://www.bleague.jp/schedule/?",
                           "tab=",
                           gsub("B", "", league),
                           "&year=",
                           substr(season, 0, 4),
                           "&event=",
-                          as.character(event.Id),
+                          as.character(eventId),
                           "&club=",
-                          as.character(team.Id),
-                          sep = "")
-        print(url.team)
-        webDr$navigate(url.team)
-        Sys.sleep(0.5)
+                          as.character(teamId))
+        print(paste(teamName,
+                    urlTeam))
+        
+        webDr$navigate(urlTeam)
+        Sys.sleep(0.5) # Consider making this event-based
         pageSource <- webDr$getPageSource()
-        html.team <- read_html(pageSource[[1]], encoding = "utf-8")
-        urls.game <- html.team %>%
+        htmlTeam <- read_html(pageSource[[1]], encoding = "utf-8")
+        
+        urlGames <- htmlTeam %>%
           html_nodes("#round_list > dd > ul > li > div.gamedata_left > div.data_link > div.state_link.btn.report > a") %>%
           html_attr("href")
-        num.games = length(urls.game)
-        print(num.games)
-        for (url.game in urls.game) {
+        for (urlGame in urlGames) {
           startStr <- "ScheduleKey="
-          key <- substring(url.game,
-                           regexpr(startStr, url.game) + nchar(startStr))
+          key <- substring(urlGame,
+                           regexpr(startStr, urlGame) + nchar(startStr))
           
           # Duplicate check
-          if (!(key %in% scheduleKeys)) {
-            scheduleKeys <- append(scheduleKeys, key)
-            webDr$navigate(url.game)
-            Sys.sleep(1)
-            pageSource2 <- webDr$getPageSource()
-            html.game <- read_html(pageSource2[[1]], encoding = "utf-8")
+          if (!(key %in% keysInData)) {
+            keysInData <- append(keysInData, key)
+            record <- .ScrapeGamePage(webDr, urlGame)
+            record$ScheduleKey <- key
+            record$Season <- season
+            record$League <- league
+            record$EventId <- eventId
             
-            ########
-            # Parsing all the necessary information
-            ########
-            date <- html.game %>%
-              html_nodes("#game__top__inner > div.date_wrap > p:nth-child(2) > span") %>%
-              html_text()
+            print(paste(record$ScheduleKey,
+                        record$Season,
+                        record$League,
+                        record$EventId,
+                        record$Date,
+                        record$Arena,
+                        record$Attendance,
+                        record$HomeTeam,
+                        record$AwayTeam))
             
-            arena <- html.game %>%
-              html_nodes("#game__top__inner > div.place_wrap > p.StadiumNameJ") %>%
-              html_text()
-            
-            attendance <- html.game %>%
-              html_nodes("#game__top__inner > div.place_wrap > p.Attendance") %>%
-              html_text()
-            
-            home <- html.game %>%
-              html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.home.win > div.team_name > p.for-sp") %>%
-              html_text()
-            if (identical(home, character(0))) {
-              home <- html.game %>%
-                html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.home > div.team_name > p.for-sp") %>%
-                html_text()
-            }
-            
-            away <- html.game %>%
-              html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.away.win > div.team_name > p.for-sp") %>%
-              html_text()
-            if (identical(away, character(0))) {
-              away <- html.game %>%
-                html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.away > div.team_name > p.for-sp") %>%
-                html_text()
-            }
-            
-            ########
-            # Create the result
-            ########
-            str <- paste(key,
-                         season,
-                         league,
-                         event.Category,
-                         event.Name,
-                         date,
-                         arena,
-                         attendance,
-                         home,
-                         away)
-            print(str)
-            
-            df.record <- data.frame(
-              ScheduleKey = key,
-              Season = season,
-              League = league,
-              EventId = event.Id,
-              Date = date,
-              Arena = arena,
-              Attendance = attendance,
-              HomeTeam = home,
-              AwayTeam = away
-            )
-            
-            df.result <- rbind(df.result, df.record)
+            result <- rbind(result, record)
           }
         }
       }
     }
   }
+  
+  result$Date <- bleaguer::GetFullDateString(result$Date, result$Season)
+
+  teams <- bleaguer::b.teams[, c("TeamId", "Season", "NameShort")]
+  result <- merge(result, teams, by.x = c("Season","HomeTeam"),by.y = c("Season","NameShort"))
+  names(result)[names(result) == 'TeamId'] <- 'HomeTeamId'
+  result <- merge(result, teams, by.x = c("Season","AwayTeam"),by.y = c("Season","NameShort"))
+  names(result)[names(result) == 'TeamId'] <- 'AwayTeamId'
+  
+  result <- result[, c("ScheduleKey",
+                       "Season",
+                       "EventId",
+                       "Date",
+                       "Arena",
+                       "Attendance",
+                       "HomeTeamId",
+                       "AwayTeamId")]
+  return(result)
 }
+
