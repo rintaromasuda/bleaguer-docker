ce4bbe8275a86888d15a7ab6c0cb9e61d2acb1f5
Rintaro Masuda <rintaromasuda@live.com>
2020-01-20 14:12:13 +0900

    Adding Github update prototype

--- bleaguer-updater/Main.R
+++ bleaguer-updater/Main.R
@@ -36,9 +36,15 @@ tryCatch({
   
   webDr$close()
 
-  system("ls")
   system("git clone https://github.com/rintaromasuda/bleaguer.git")
+  setwd("/bleaguer")
+  system("git checkout -b user/rintarom/test2")
+  system("cat DESCRIPTION > test.txt")
+  system("echo \"tete\" > foo.txt ")
   system("ls")
+  system("git add .")
+  system("git commit -m \"My comment\"")
+  system("git push --set-upstream origin user/rintarom/test2")
 },
 error = function(e){
   print(e)
