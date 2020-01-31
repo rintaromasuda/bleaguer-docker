aef460d5246751b501a9fbb208cbb7894751d352
Rintaro Masuda <rintaromasuda@live.com>
2020-01-31 10:05:02 +0900

    GitUpdate in progress

--- bleaguer-updater/UpdateGithub.R
+++ bleaguer-updater/UpdateGithub.R
@@ -1,7 +1,16 @@
 UpdateGithub <- function(targetSeason){
+  library(stringr)
+  branchName <- paste0("update/", format(Sys.time(), "%Y%m%d%H%M%S"))
+
   system("git clone https://github.com/rintaromasuda/bleaguer.git")
-  # setwd("/bleaguer")
-  # system("git checkout -b user/rintarom/test2")
+  setwd("/bleaguer")
+  system(paste("git checkout -b", branchName)
+
+  season <- stringr::str_replace(targetSeason, "-", "")
+  targetGamesFile <- paste0("inst/extdata/games_", season, ".csv")
+  targetSummaryFile <- paste0("inst/extdata/games_summary_", season, ".csv")
+  targetBoxscoreFile <- paste0("inst/extdata/games_boxscore_", season, ".csv")
+  
   # system("cat DESCRIPTION > test.txt")
   # system("echo \"tete\" > foo.txt ")
   # system("ls")
